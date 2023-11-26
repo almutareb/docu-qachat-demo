cb2af978216a135684cba0a2f18b3eac165ce164
Asaad Almutareb <asaad@mutareb.com>
2023-11-26 16:21:36 +0000

    adjusted embeddings model for better results!

--- app.py
+++ app.py
@@ -15,35 +15,48 @@ from langchain.llms import HuggingFaceHub
 from langchain.embeddings import HuggingFaceHubEmbeddings
 # vectorestore
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
 # retrieval chain
 from langchain.chains import RetrievalQA
-from langchain.chains import RetrievalQAWithSourcesChain
 # prompt template
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 # logging
-import logging
+#import logging
+import zipfile
 
 # load HF Token
 config = load_dotenv(".env")
 
 
-model_id = HuggingFaceHub(repo_id="HuggingFaceH4/zephyr-7b-beta", model_kwargs={"temperature":0.1, "max_new_tokens":1024, "repetition_penalty":1.2})
+model_id = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.1", model_kwargs={"temperature":0.1, "max_new_tokens":1024, "repetition_penalty":1.2, "streaming": True, "return_full_text":True})
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-embeddings = HuggingFaceHubEmbeddings()
+model_name = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
+embeddings = HuggingFaceHubEmbeddings(repo_id=model_name)
+
+
 
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-s3.download_file('rad-rag-demos', 'vectorstores/chroma.sqlite3', './chroma_db/chroma.sqlite3')
 
+## Chroma DB
+s3.download_file('rad-rag-demos', 'vectorstores/chroma.sqlite3', './chroma_db/chroma.sqlite3')
 db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 db.get()
 
+## FAISS DB
+# s3.download_file('rad-rag-demos', 'vectorstores/faiss_db_ray.zip', './chroma_db/faiss_db_ray.zip')
+# with zipfile.ZipFile('./chroma_db/faiss_db_ray.zip', 'r') as zip_ref:
+#     zip_ref.extractall('./chroma_db/')
+
+# FAISS_INDEX_PATH='./chroma_db/faiss_db_ray'
+# db = FAISS.load_local(FAISS_INDEX_PATH, embeddings)
+
 retriever = db.as_retriever(search_type = "mmr")
+#(search_type="similarity_score_threshold", search_kwargs={'score_threshold': 0.8}) #(search_type = "mmr", search_kwargs={'k': 5, 'fetch_k': 25})
 global qa 
 template = """
-Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question:
+You are the friendly documentation buddy Arti, who helps users in using RAY, the open-source unified framework for scaling AI and Python applications.\
+    Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question :
 ------
 <ctx>
 {context}
@@ -75,7 +88,6 @@ def add_text(history, text):
 
 def bot(history):
     response = infer(history[-1][0], history)
-    print(*response)
     print(*memory)
     sources = [doc.metadata.get("source") for doc in response['source_documents']]
     src_list = '\n'.join(sources)
@@ -86,6 +98,8 @@ def bot(history):
         history[-1][1] += character
         time.sleep(0.05)
         yield history
+    # history[-1][1] = response['result']
+    # return print_this
 
 def infer(question, history):
     query =  question
